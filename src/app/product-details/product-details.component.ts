8ccadbfc7a013dceff7c304c25f79d60d36a3a72
Priya Rama <priyaalq@gmail.com>
2022-02-25 14:37:00 +0100

    third part

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,9 @@ export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
   /* ... */
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private cartService: CartService
+              ) { }            
 
   ngOnInit(): void {
 
@@ -21,8 +24,13 @@ export class ProductDetailsComponent implements OnInit {
   const productIdFromRoute = Number(routeParams.get('productId'));
 
   // Find the product that correspond with the id provided in route.
-  this.product = products.find(product => product.id === productIdFromRoute);
-  
+  this.product = products.find(product => product.id === productIdFromRoute);  
+
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 
 }
